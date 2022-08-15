5da7ffd8619a7f892ca85cb7fe5b9dc78d231a5c
NT <usedforfunny@gmail.com>
2022-08-15 19:16:28 +0200

    Initial Commit

---
+++ KcalCalc/KcalCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  KcalCalcApp.swift
+//  KcalCalc
+//
+//  Created by Alexandre Talatinian on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct KcalCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
